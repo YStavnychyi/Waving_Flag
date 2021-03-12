0d307e59c2a0b89dcf7c7a445748a4b4609769fd
Yevhenii <zhestav20@gmail.com>
2021-03-12 22:57:37 +0100

    On development stage

--- Flag/Source.cpp
+++ Flag/Source.cpp
@@ -1,215 +1,211 @@
+﻿#define _USE_MATH_DEFINES
+#define BITMAP_ID 0x4D42
+
 #include <iostream>
 #include <glut.h>
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
 using namespace std;
 
-HGLRC           hRC = NULL;                           // Permanent Rendering Context
-HDC             hDC = NULL;                           // Private GDI Device Context
-HWND            hWnd = NULL;                          // Holds Our Window Handle
-HINSTANCE       hInstance;                          // Holds The Instance Of The Application
+//Zmienne globalne
+HDC g_HDC;								//globalny kontekst urzdządzenia
+bool fullScreen = false;				//true = tryb pełnoekrankowy
+										//false = tryb okienkowy
+bool keyPressed[256];					//tablica przyciśnięć klawisz
 
-bool    keys[256];                              // Array Used For The Keyboard Routine
-bool    active = TRUE;                            // Window Active Flag Set To TRUE By Default
-bool    fullscreen = TRUE;                        // Fullscreen Flag Set To Fullscreen Mode By Default
+//Opis tekstury
+BITMAPINFOHEADER bitmapInfoHeader;		//naglówek pliku
+unsigned char* bitmapData;				//dane tekstury
+unsigned int texture;					//obiekt tekstury
+//Opis flagi
+float flagPoints[36][20][3];			//flaga 36x20
+float wrapValue;						//przenosi fali z końca na początek flagi
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//Inicjacja punktów flagi za pomocą funkcji sin()
+void InitializeFlag()
+{
+	int x;								//licznik w plaszczyżnie x
+	int y;								//licznik w plaszczyżnie y
+	float sinTemp;						
 
-GLvoid ReSizeScene(GLsizei width, GLsizei height) {  // Resize And Initialize The GL Window
-    if (height == 0) {
-        height = 1;
+	//Przegląda wszystkie punkty flagi w pętli i wyznacza wartość funkcji sin dla
+	//współrzędnej z. Wszpółrzędne x i y otrzymują wartość odpowiedniego licznika pętli
+	for (x = 0; x < 36; x++)
+	{
+		for (y = 0; y < 20; y++)
+		{
+			flagPoints[x][y][0] = (float)x;
+			flagPoints[x][y][1] = (float)y;
+			sinTemp = (((float)x * 20.0f) / 360.0f) * 2.0f * M_PI;
+			flagPoints[x][y][2] = (float)sin(sinTemp);
+		}
+	}
+}
+
+unsigned char *LoadBitmapFile(const char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
+{
+    FILE* filePtr;                      //wskażnik pliku
+    BITMAPFILEHEADER bitmapFileHeader;  //naglówek pliku
+    unsigned char* bitmapImage;         //bufor obrazu
+    int imageIdx = 0;                   //licznik bajtów obrazu
+    unsigned char tempRGB;              //zmienna zamiany składowych
+
+    //otwiera plik w trybie read binary
+    filePtr = fopen(filename, "rb");
+    if (filePtr == NULL)
+        return NULL;
+
+    //wczytuje nagłówek pliku
+    fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
+
+    //sprawdza, czy rzeczywiście jest to plik BMP
+    if (bitmapFileHeader.bfType != BITMAP_ID)
+    {
+        fclose(filePtr);
+        return NULL;
     }
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+
+    //wczytuje nagłówek obrazu zapisanego w pliku
+    fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr); 
+
+    //ustawia wskażnik pliku na początku danych opisujących obraz
+    fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET);
+
+    //przydziela pamięć na bufor obrazu
+    bitmapImage = (unsigned char*)malloc(bitmapInfoHeader->biSizeImage);
+
+    //sprawdza, czy pamięć została przydzielona
+    if (!bitmapImage)
+    {
+        free(bitmapImage);
+        fclose(filePtr);
+        return NULL;
+    }
+
+    //wczytuje dane obrazu
+    fread(bitmapImage, bitmapInfoHeader->biSizeImage, 1, filePtr);
+
+
+    //sprawdza czy operacja powiodła się
+    if (bitmapImage == NULL)
+    {
+        fclose(filePtr);
+        return NULL;
+    }
+
+    //zamienia składowe R i B, aby uzyskać format RGB
+    for (imageIdx = 0; imageIdx < bitmapInfoHeader->biSizeImage; imageIdx += 3)
+    {
+        tempRGB = bitmapImage[imageIdx];
+        bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
+        bitmapImage[imageIdx + 2] = tempRGB;
+    }
+
+    //zamyka plik i zwraca wskażnik bufora zawierającego obraz
+    fclose(filePtr);
+    return bitmapImage;
 }
 
-int InitGL(GLvoid) {                                 // All Setup For OpenGL Goes Here
-    glShadeModel(GL_SMOOTH);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClearDepth(1.0f);
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    return true;
+//Zainicjowania grafiki OpenGl
+void Initialize()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	//tlo w czarnym kolorze
+	glEnable(GL_DEPTH_TEST);				//usuwanie ukrytych powierzchni
+	glEnable(GL_CULL_FACE);					//brak obliczeń dla niewidocznych stron wielokątów
+	glFrontFace(GL_CCW);					//niewidoczne strony posiadają porządek wierzcholków
+											//przeciwny do kierunku ruchu wskazówek zegara
+	glEnable(GL_TEXTURE_2D);				//wlącza tekstury 2D
+
+    //ładuje obraz tekstury
+    bitmapData = LoadBitmapFile("flag.bmp", &bitmapInfoHeader);
+
+    glGenTextures(1, &texture);             //tworzy obiekt tekstury
+    glBindTexture(GL_TEXTURE_2D, texture);  //aktywuje obiekt tekstury
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    //tworzy obraz tekstury
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, bitmapData);
+    InitializeFlag();
 }
 
-int DrawGLScene(GLvoid) {                            // Here's Where We Do All The Drawing
+//Obraz flagi dla pojedynczej klatki animacjii
+void DrawFlag()
+{
+    int x;                                  //licznik w kierunku osi x
+    int y;                                  //licznik w kierunku osi y
+    float texLeft;                          //lewa współrzędna tekstury
+    float texBottom;                        //dolna współrzedna tekstury
+    float texTop;                           //górna współrzedna tekstury
+    float texRight;                         //prawa współrzędna tekstury
+
+    glPushMatrix();
+    glBindTexture(GL_TEXTURE_2D, texture);  //wybiera obiekt tekstury
+    glBegin(GL_QUADS);                      //rysuje czworokąty
+
+    for (x = 0; x < 36; x++)
+    {
+        for (y = 0; y < 18; y++)
+        {
+            //wyznacza współrzędne tekstury dla bieżącego czworokąta
+            texLeft = float(x) / 35.0f;
+            texBottom = float(y) / 18.0f;
+            texRight = float(x + 1) / 35.0f;
+            texTop = float(y + 1) / 18.0f;
+
+            //lewy dolny wierzcholek
+            glTexCoord2f(texLeft, texBottom);
+            glVertex3f(flagPoints[x][y][0], flagPoints[x][y][1], flagPoints[x][y][2]);
+
+            //prawy dolny wierzcholek
+            glTexCoord2f(texRight, texBottom);
+            glVertex3f(flagPoints[x+1][y][0], flagPoints[x+1][y][1], flagPoints[x+1][y][2]);
+
+            //prawy górny wierzcholek
+            glTexCoord2f(texRight, texTop);
+            glVertex3f(flagPoints[x+1][y+1][0], flagPoints[x+1][y+1][1], flagPoints[x+1][y+1][2]);
+
+            //lewy górny wierzcholek
+            glTexCoord2f(texLeft, texTop);
+            glVertex3f(flagPoints[x][y+1][0], flagPoints[x][y+1][1], flagPoints[x][y+1][2]);
+        }
+    }
+    glEnd();
+
+    //Udaje ruch flagi
+    for (y = 0; y < 19; y++)
+    {
+        wrapValue = flagPoints[35][y][2];
+        for (x = 35; x < 0; x--)
+        {
+            flagPoints[x][y][2] = flagPoints[x - 1][y][2];
+        }
+        flagPoints[0][y][2] = wrapValue;
+    }
+    glPopMatrix();
+}
+
+//Przesunięcie układu współrzędnych
+void Render()
+{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    return true;
+
+    glTranslatef(-15.0f, -10.0f, -50.0f);               //wykonuje przesunięcie
+    DrawFlag();                                         //rysuje flagę
+    glFlush();
+    SwapBuffers(g_HDC);                                 //przełącza bufory
 }
 
-GLvoid KillGLWindow(GLvoid) {                        // Properly Kill The Window
-    if (fullscreen) {
-        ChangeDisplaySettings(NULL, 0);
-        ShowCursor(true);
-    }
-    if (hRC) {
-        if (!wglMakeCurrent(NULL, NULL)) {
-            MessageBox(NULL, L"Release Of DC And RC Failed.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
-        }
-        if (!wglDeleteContext(hRC)) {
-            MessageBox(NULL, L"Release Rendering Context Failed.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
-        }
-        hRC = NULL;
-    }
-    if (hDC && !ReleaseDC(hWnd, hDC)) {
-        MessageBox(NULL, L"Release Device Context Failed.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
-        hDC = NULL;
-    }
-    if (hWnd && !DestroyWindow(hWnd)) {
-        MessageBox(NULL, L"Could Not Release hWnd.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
-        hWnd = NULL;
-    }
-    if (!UnregisterClass(L"OpendGL", hInstance)) {
-        MessageBox(NULL, L"Could Not Unregister Class.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
-        hInstance = NULL;
-    }
-}
-
-bool CreateGLWindow(char* title, int width, int height, int bits, bool fullscreenflag) {
-    GLint PixelFormat;
-    WNDCLASS wc;
-    DWORD dwExStyle;
-    DWORD dwStyle;
-
-    RECT WindowRect;
-    WindowRect.left = (long)0;
-    WindowRect.right = (long)width;
-    WindowRect.top = (long)0;
-    WindowRect.bottom = (long)height;
-
-    fullscreen = fullscreenflag;
-
-    hInstance = GetModuleHandle(NULL);
-    wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-    wc.lpfnWndProc = (WNDPROC)WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = NULL;
-    wc.lpszMenuName = NULL;
-    wc.lpszClassName = L"OpenGL";
-
-    if (!RegisterClass(&wc)) {
-        MessageBox(NULL, L"Failed To Register The Window Class.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return false;
-    }
-    if (fullscreen) {
-        DEVMODE dmScreenSettings;
-        memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
-        dmScreenSettings.dmSize = sizeof(dmScreenSettings);
-        dmScreenSettings.dmPelsWidth = width;
-        dmScreenSettings.dmPelsHeight = height;
-        dmScreenSettings.dmBitsPerPel = bits;
-        dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
-        if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL) {
-            if (MessageBox(NULL, L"The Requested Fullscreen Mode Is Not Supported By\nYour Video Card. Use Windowed Mode Instead?", L"GL", MB_YESNO | MB_ICONEXCLAMATION) == IDYES) {
-                fullscreen = false;
-            }
-            else {
-                MessageBox(NULL, L"Program Will Now Close.", L"ERROR", MB_OK | MB_ICONSTOP);
-                return FALSE;
-            }
-        }
-    }
-    if (fullscreen) {
-        dwExStyle = WS_EX_APPWINDOW;
-        dwStyle = WS_POPUP;
-        ShowCursor(FALSE);
-    }
-    else {
-        dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
-        dwStyle = WS_OVERLAPPEDWINDOW;
-    }
-    AdjustWindowRectEx(&WindowRect, dwStyle, FALSE, dwExStyle);
-    if (!(hWnd = CreateWindowEx(dwExStyle, L"OpenGL", L"Flag", WS_CLIPSIBLINGS | WS_CLIPCHILDREN | dwStyle, 0, 0, WindowRect.right - WindowRect.left, WindowRect.bottom - WindowRect.top, NULL, NULL, hInstance, NULL))) {
-        KillGLWindow();
-        MessageBox(NULL, L"Window Creation Error.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return false;
-    }
-
-    static  PIXELFORMATDESCRIPTOR pfd =                  // pfd Tells Windows How We Want Things To Be
-    {
-        sizeof(PIXELFORMATDESCRIPTOR),
-        1,
-        PFD_DRAW_TO_WINDOW |
-        PFD_SUPPORT_OPENGL |
-        PFD_DOUBLEBUFFER,
-        PFD_TYPE_RGBA,
-        16,
-        0, 0, 0, 0, 0, 0,
-        0,
-        0,
-        0,
-        0, 0, 0, 0,
-        16,
-        0,
-        0,
-        PFD_MAIN_PLANE,
-        0,
-        0, 0, 0
-    };
-
-    if (!(hDC = GetDC(hWnd))) {
-        KillGLWindow();
-        MessageBox(NULL, L"Can't Create A GL Device Context.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return false;
-    }
-
-    if (!(PixelFormat = ChoosePixelFormat(hDC, &pfd))) {
-        KillGLWindow();
-        MessageBox(NULL, L"Can't Find A Suitable PixelFormat.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return false;
-    }
-
-    if (!SetPixelFormat(hDC, PixelFormat, &pfd)) {
-        KillGLWindow();
-        MessageBox(NULL, L"Can't Set The PixelFormat.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return false;
-    }
-
-    if (!(hRC = wglCreateContext(hDC))) {
-        KillGLWindow();
-        MessageBox(NULL, L"Can't Create A GL Rendering Context.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return FALSE;
-    }
-
-    if (!wglMakeCurrent(hDC, hRC)) {
-        KillGLWindow();
-        MessageBox(NULL, L"Can't Activate The GL Rendering Context.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return FALSE;
-    }
-
-    ShowWindow(hWnd, SW_SHOW);
-    SetForegroundWindow(hWnd);
-    SetFocus(hWnd);
-    ReSizeScene(width, height);
-
-    if (!InitGL()) {
-        KillGLWindow();
-        MessageBox(NULL, L"Initialization Failed.", L"ERROR", MB_OK | MB_ICONEXCLAMATION);
-        return false;
-    }
-
-    return true;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+//funkcja określająca format pikseli
+void SetupPixelFormat(HDC hDC)
 {
 
-    return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
-int Winmain()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
