1fd45fcfa3159561f093f76cdc759460caaa4809
Yevhenii <zhestav20@gmail.com>
2021-03-27 17:21:36 +0100

    Falowanie flagi

--- Flag/Source.cpp
+++ Flag/Source.cpp
@@ -40,6 +40,7 @@ void InitializeFlag()
     {
         for (yIdx = 0; yIdx < 20; yIdx++)
         {
+
             flagPoints[xIdx][yIdx][0] = (float)xIdx;
             flagPoints[xIdx][yIdx][1] = (float)yIdx;
             sinTemp = (((float)xIdx * 20.0f) / 360.0f) * 2.0f * M_PI;
@@ -47,98 +48,7 @@ void InitializeFlag()
         }
     }
 }
-/*
-unsigned char *LoadBitmapFile(const char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
-{
-    FILE* filePtr;                      //wskażnik pliku
-    BITMAPFILEHEADER bitmapFileHeader;  //naglówek pliku
-    unsigned char* bitmapImage;         //bufor obrazu
-    int imageIdx = 0;                   //licznik bajtów obrazu
-    unsigned char tempRGB;              //zmienna zamiany składowych
-    //otwiera plik w trybie read binary
-    fopen_s(&filePtr, filename, "rb");
-    //filePtr = fopen(filename, "rb");
-    if (filePtr == NULL)
-        return NULL;
-    //wczytuje nagłówek pliku
-    fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
-    //sprawdza, czy rzeczywiście jest to plik BMP
-    if (bitmapFileHeader.bfType != BITMAP_ID)
-    {
-        fclose(filePtr);
-        return NULL;
-    }
-    //wczytuje nagłówek obrazu zapisanego w pliku
-    fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr);
-    //ustawia wskażnik pliku na początku danych opisujących obraz
-    fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET);
-    //przydziela pamięć na bufor obrazu
-    bitmapImage = (unsigned char*)malloc(bitmapInfoHeader->biSizeImage);
-    //sprawdza, czy pamięć została przydzielona
-    if (!bitmapImage)
-    {
-        free(bitmapImage);
-        fclose(filePtr);
-        return NULL;
-    }
-    //wczytuje dane obrazu
-    fread(bitmapImage, bitmapInfoHeader->biSizeImage, 1, filePtr);
-    //sprawdza czy operacja powiodła się
-    if (bitmapImage == NULL)
-    {
-        fclose(filePtr);
-        return NULL;
-    }
-    //zamienia składowe R i B, aby uzyskać format RGB
-    for (imageIdx = 0; imageIdx < bitmapInfoHeader->biSizeImage; imageIdx += 3)
-    {
-        tempRGB = bitmapImage[imageIdx];
-        bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
-        bitmapImage[imageIdx + 2] = tempRGB;
-    }
-    //zamyka plik i zwraca wskażnik bufora zawierającego obraz
-    fclose(filePtr);
-    return bitmapImage;
-}
-*/
 
-
-/*
-GLuint LoadTexture(const char* filename)
-{
-    GLuint texture;
-    int width, height;
-    unsigned char* data;
-    FILE* file;
-    fopen_s(&file, filename, "rb");
-    if (file == NULL) return 0;
-    width = 1024;
-    height = 512;
-    data = (unsigned char*)malloc(width * height * 3);
-    //int size = fseek(file,);
-    fread(data, width * height * 3, 1, file);
-    fclose(file);
-    for (int i = 0; i < width * height; ++i)
-    {
-        int index = i * 3;
-        unsigned char B, R;
-        B = data[index];
-        R = data[index + 2];
-        data[index] = R;
-        data[index + 2] = B;
-    }
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
-    free(data);
-    return texture;
-}
-*/
 //Zainicjowania grafiki OpenGl
 void Initialize()
 {
@@ -146,50 +56,27 @@ void Initialize()
     glEnable(GL_DEPTH_TEST);				//usuwanie ukrytych powierzchni
     glDepthFunc(GL_LESS);
     glShadeModel(GL_SMOOTH);
-    //glEnable(GL_LIGHTING);
-    //glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
     glEnable(GL_CULL_FACE);					//brak obliczeń dla niewidocznych stron wielokątów
     glFrontFace(GL_CCW);					//niewidoczne strony posiadają porządek wierzcholków
                                             //przeciwny do kierunku ruchu wskazówek zegara
     glEnable(GL_TEXTURE_2D);				//wlącza tekstury 2D
 
     //ładuje obraz tekstury
-    //bitmapData = LoadBitmapFile("flag.bmp", &bitmapInfoHeader);
-
     int width, height, nrChannels;
     bitmapData = stbi_load("flag.bmp", &width, &height, &nrChannels, STBI_rgb_alpha);
 
     glGenTextures(1, &texture);             //tworzy obiekt tekstury
     glBindTexture(GL_TEXTURE_2D, texture);  //aktywuje obiekt tekstury
     gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, width, height, GL_RGBA, GL_UNSIGNED_BYTE, bitmapData);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
     InitializeFlag();
-
-           
- /*
- if (bitmapData)
-    {
-        //tworzy obraz tekstury
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, bitmapData);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bitmapInfoHeader.biWidth, bitmapInfoHeader.biHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, bitmapData);
-        //stbi_image_free(bitmapData);
-
-        InitializeFlag();
-    }
-    else
-    {
-        printf("Error!");
-        MessageBox(NULL, L"Failed to load texture", NULL, MB_OK);
-    }
-*/
-   
-
 }
 
 //Obraz flagi dla pojedynczej klatki animacjii
